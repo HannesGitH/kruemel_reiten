59eb0bf808af112b60c7375a5647a46c7b5adbf0
hannesVonSarahAus <hanneselias2@gmail.com>
2020-03-22 14:25:39 +0100

    database setup

--- lib/other/Database.dart
+++ lib/other/Database.dart
@@ -3,3 +3,81 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'dart:async';
 
+class TheDatabase {
+  static final TheDatabase _instance = TheDatabase._();
+  static Database _database;
+
+  TheDatabase._();
+
+  factory TheDatabase() {
+    return _instance;
+  }
+
+
+  Future<Database> get db async {
+    if (_database != null) {
+      return _database;
+    }
+    _database = await init();
+
+    return _database;
+  }
+
+  Future<Database> init() async {
+    String dbPath = join(await getDatabasesPath(), 'calenderDatabase.db');
+    var database = openDatabase(
+        dbPath, version: 1, onCreate: _onCreate, onUpgrade: _onUpgrade);
+    return database;
+  }
+
+  void _onCreate(Database db, int version) {
+    db.execute('''
+    CREATE TABLE user(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT,
+      tel TEXT)
+    ''');
+
+    db.execute('''
+    CREATE TABLE group(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      kid1 INTEGER,
+      kid2 INTEGER,
+      kid3 INTEGER,
+      name TEXT,
+      isSec INTEGER)
+    ''');
+
+    db.execute('''
+    CREATE TABLE lesson(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      kid INTEGER,
+      presence INTEGER,
+      paymentStatus INTEGER,
+      description TEXT)
+    ''');
+
+    db.execute('''
+    CREATE TABLE payment(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      kid INTEGER,
+      cents INTEGER,
+      date INTEGER,
+      extra TEXT)
+    ''');
+
+  }
+
+  void _onUpgrade(Database db, int oldVersion, int newVersion) {
+    // Run migration according database versions
+  }
+
+}
+
+enum presence {
+  future,
+  wasThere,
+  canceledInTime,
+  canceledJust,
+  canceledNot
+}
