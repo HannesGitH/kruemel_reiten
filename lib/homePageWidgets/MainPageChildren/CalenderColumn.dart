271a1c2a63d3c9d99be57b330f270573331af9d2
hannesVonSarahAus <hanneselias2@gmail.com>
2020-03-30 01:35:54 +0200

    calendercolumn created , not even a stump

---
+++ lib/homePageWidgets/MainPageChildren/CalenderColumn.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CalenderColumn extends StatelessWidget{
+  DateTime date;
+
+  CalenderColumn({@required this.date});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
